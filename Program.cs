44bb53ce57b4f63ff0a8a144c224592428eb0dea
Kandr9 <kandr13579@gmail.com>
2023-06-30 10:16:42 +0300

    10

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// №10
+
+System.Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ int x=num%100;
+if (num / 100 >= 1 && num/1000 <1)
+
+    System.Console.Write( x / 10);
+    else 
+    System.Console.Write("ВВедёное число не трёхзначное");
